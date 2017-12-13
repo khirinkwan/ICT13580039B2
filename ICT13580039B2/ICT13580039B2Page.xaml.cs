da8a6951ba3fda3d52f17c85f8aaac9f8f6649db
dyta <18145142+dyta@users.noreply.github.com>
2017-12-13 14:57:59 +0700

    งาน2

---
+++ ICT13580039B2/ICT13580039B2Page.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace ICT13580039B2
+{
+    public partial class ICT13580039B2Page : ContentPage
+    {
+        public ICT13580039B2Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
